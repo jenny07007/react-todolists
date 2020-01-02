7342f5314f98ec019c33a70b3cdab830afdc8f44
jenny07007 <jennyliu07@gmail.com>
2020-01-02 10:48:40 +0800

    Updates

---
+++ precache-manifest.e9fda8b18cfc3f793fc2a5fd0d6c9673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6b1dbf40b4b5627965775a01fe1675",
+    "url": "/react-todolists/index.html"
+  },
+  {
+    "revision": "a5f3ffec11a740f14d6b",
+    "url": "/react-todolists/static/css/main.bc20196a.chunk.css"
+  },
+  {
+    "revision": "fb3cf3c536022a8b89ef",
+    "url": "/react-todolists/static/js/2.af5ddad4.chunk.js"
+  },
+  {
+    "revision": "a5f3ffec11a740f14d6b",
+    "url": "/react-todolists/static/js/main.6a9d56bf.chunk.js"
+  },
+  {
+    "revision": "8b1975139fff88957869",
+    "url": "/react-todolists/static/js/runtime~main.f8686e04.js"
+  },
+  {
+    "revision": "ccca7275f90d7cbb823db337a2bdce2d",
+    "url": "/react-todolists/static/media/bk.ccca7275.jpg"
+  }
+]);
