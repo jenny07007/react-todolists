7c4b379054ad84db6c7015de6f0de793f660b18a
jenny07007 <jennyliu07@gmail.com>
2022-04-12 10:31:25 +0800

    update dependencies

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,49 +5,48 @@ import { Todo } from "./Todo";
 export class TodoList extends Component {
   state = { todos: [] };
 
-  addItem = task => {
+  addItem = (task) => {
     this.setState({
-      todos: [...this.state.todos, task]
+      todos: [...this.state.todos, task],
     });
   };
 
-  deleteItem = id => {
+  deleteItem = (id) => {
     this.setState({
-      todos: this.state.todos.filter(task => task.id !== id)
+      todos: this.state.todos.filter((task) => task.id !== id),
     });
   };
 
   updateItem = (id, updatedTask) => {
-    const updatedTodos = this.state.todos.map(todo => {
+    const updatedTodos = this.state.todos.map((todo) => {
       return todo.id === id ? { ...todo, task: updatedTask } : todo;
     });
     this.setState({ todos: updatedTodos });
   };
 
-  toggleCompletion = id => {
-    const updatedTodos = this.state.todos.map(todo => {
+  toggleCompletion = (id) => {
+    const updatedTodos = this.state.todos.map((todo) => {
       return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
     });
     this.setState({ todos: updatedTodos });
   };
 
   render() {
-    const todos = this.state.todos.map(todo =>
-      !todo.task && !todo.id ? (
-        ""
-      ) : (
-        <li>
-          <Todo
-            id={todo.id}
-            key={todo.id}
-            task={todo.task}
-            completed={todo.completed}
-            handleDeleteItem={() => this.deleteItem(todo.id)}
-            handleCompletion={() => this.toggleCompletion(todo.id)}
-            handleUpdateItem={this.updateItem.bind(this)}
-          />
-        </li>
-      )
+    const todos = this.state.todos.map(
+      (todo) =>
+        todo.task &&
+        todo.id && (
+          <li key={todo.id}>
+            <Todo
+              id={todo.id}
+              task={todo.task}
+              completed={todo.completed}
+              handleDeleteItem={() => this.deleteItem(todo.id)}
+              handleCompletion={() => this.toggleCompletion(todo.id)}
+              handleUpdateItem={this.updateItem.bind(this)}
+            />
+          </li>
+        ),
     );
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { TodoList } from "./TodoList";
 import "./styles.css";
@@ -13,4 +13,5 @@ function App() {
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const root = createRoot(rootElement);
+root.render(<App />);
